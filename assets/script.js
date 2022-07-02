d822d497737d3bb18398233b93ec1c47f4041538
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-01 22:02:24 -0700

    add event listener to search button and append city info

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//creates and appends city name, date, and information to container
+//creates and appends city name, date, and information to weather container
 var date = moment().format("MMM, Do, YYYY");
 var weatherContainer = document.querySelector(".weather-container");
 var cityName = document.createElement("h3");
@@ -9,8 +9,33 @@ var weatherContent = document.createElement("p")
 weatherContent.textContent = "icon, temperature, wind, humidity, UV index with color"
 weatherContainer.append(weatherContent)
 
-// next task
+// creates and appends information to forecast container
 var forecastContainer = document.querySelector(".forecast")
 var forecast = document.createElement("h3")
 forecast.textContent = "5-Day Forecast:"
 forecastContainer.append(forecast)
+
+// adds on click event to search button and appends cities as buttons in the container
+var searchButton = document.querySelector(".btn")
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    var buttonContainer = document.querySelector(".button-container")
+    var newCity = document.createElement("button")
+    var userInput = document.querySelector(".form-control").value
+
+    if (userInput == "") {
+        return;
+    } else {
+        newCity.textContent = userInput
+        buttonContainer.append(newCity)
+    } ClearFields()
+})
+
+// clears the input field after search button is clicked
+function ClearFields() {
+    document.querySelector(".form-control").value = "";
+}
+
+function localStorage() {
+
+}
