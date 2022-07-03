e3e89b6a18ee87c9da3c64f6fae13d2c98b8190b
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-03 10:58:55 -0700

    add CSS styles in HTML

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,8 @@ weatherContainer.append(weatherContent)
 forecastContent.textContent = "5-Day Forecast:"
 forecastContainer.append(forecastContent)
 
+
+//how can I use submit instead of click to use the enter key as well?******************************************
 // adds on click event to search button and appends cities as buttons in the container
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
@@ -26,15 +28,15 @@ searchButton.addEventListener("click", function(event) {
     var userInput = document.querySelector(".form-control").value
 
     if (userInput == "") {
-        var extraText = document.querySelector(".extra-text")
-        extraText.textContent = "please enter a city"
-        //how do I get this text to disappear once a new city is searched????????????????????????????????????????
+        var blankInput = document.querySelector(".blank-input")
+        blankInput.textContent = "please enter a city"
+        //how do I get this text to disappear once a new city is searched?*************************************
     } else {
         newCity.textContent = userInput
         buttonContainer.append(newCity)
     } ClearFields();
-    //how do I get this to persist on the page?????????????????????????????????????????????????????????????????????
-    localStorage.setItem("city", userInput)
+    //how do I get this to persist on the page?*****************************************************************
+    localStorage.setItem("city", JSON.stringify(userInput))
 })
 
 // clears the input field after search button is clicked
@@ -43,6 +45,18 @@ function ClearFields() {
 }
 
 //displays items in local storage
-for (var i=0; i<localStorage.length; i++) {
-    userInput = localStorage.getItem("city")
-}
+buttonContainer.textContent = JSON.parse(localStorage.getItem("city"))
+
+
+  
+
+
+
+
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=London&{YOUR_API_KEY}&appid=7acb10b31a225ce5f6e678b28717604c')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+//   api.openweathermap.org/data/2.5/weather?q=London&{YOUR_API_KEY}
+//   7acb10b31a225ce5f6e678b28717604c
