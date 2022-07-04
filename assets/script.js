ad60eab1067801a1763933198e62157d6accf4d4
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-04 11:08:55 -0700

    add api keys

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var cityName = document.createElement("h3");
 var forecastContainer = document.querySelector(".forecast")
 var forecastContent = document.createElement("h3")
 var buttonContainer = document.querySelector(".button-container")
-var searchButton = document.querySelector(".btn")
+var searchButton = document.querySelector("#searchCity")
 var userInput = document.querySelector(".form-control")
 
 // creates and appends information to forecast container
@@ -20,22 +20,19 @@ forecastContent.textContent = "5-Day Forecast:"
 forecastContainer.append(forecastContent)
 
 
-//how can I use submit instead of click to use the enter key as well?******************************************
-// adds on click event to search button and appends cities as buttons in the container
-searchButton.addEventListener("click", function(event) {
+//adds on click event to search button and appends cities as buttons in the container
+searchButton.addEventListener("submit", function(event) {
     event.preventDefault();
     var newCity = document.createElement("button")
     var userInput = document.querySelector(".form-control").value
-
+    currentWeatherData(userInput)
     if (userInput == "") {
         var blankInput = document.querySelector(".blank-input")
         blankInput.textContent = "please enter a city"
-        //how do I get this text to disappear once a new city is searched?*************************************
     } else {
         newCity.textContent = userInput
         buttonContainer.append(newCity)
     } ClearFields();
-    //how do I get this to persist on the page?*****************************************************************
     localStorage.setItem("city", JSON.stringify(userInput))
 })
 
@@ -45,18 +42,23 @@ function ClearFields() {
 }
 
 //displays items in local storage
-buttonContainer.textContent = JSON.parse(localStorage.getItem("city"))
+buttonContainer.textContent = JSON.parse(localStorage.getItem("city")) || []
 
+//api fetch by city
+function currentWeatherData(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7acb10b31a225ce5f6e678b28717604c`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        latLonData(lat, lon, cityName)
+      })
+    }
 
-  
-
-
-
-
-
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=London&{YOUR_API_KEY}&appid=7acb10b31a225ce5f6e678b28717604c')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-//   api.openweathermap.org/data/2.5/weather?q=London&{YOUR_API_KEY}
-//   7acb10b31a225ce5f6e678b28717604c
+//api fetch by lat and lon
+function latLonData(lat, lon, cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=7acb10b31a225ce5f6e678b28717604c`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
