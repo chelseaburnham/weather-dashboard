0bbe1580d4b88e02af37c1b41077db1a94e3d25b
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-04 20:46:01 -0700

    restructure HTML, CSS, and JS

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,54 @@
-//variables to target and create elements
+//appends date
 var date = moment().format("MMM Do, YYYY");
+var dateContainer = document.getElementById("date-heading")
+dateContainer.append(date)
 
-var weatherContainer = document.querySelector(".weather-container");
-var weatherContent = document.createElement("p")
-var cityName = document.createElement("h3");
-var forecastContainer = document.querySelector(".forecast")
-var forecastContent = document.createElement("h3")
-var buttonContainer = document.querySelector(".button-container")
-var searchButton = document.querySelector("#searchCity")
-var userInput = document.querySelector(".form-control")
+var userInput = document.querySelector(".form-control").value
+var searchButton = document.querySelector(".btn")
 
-// creates and appends information to forecast container
-cityName.textContent = "City Name:"
-weatherContainer.append(cityName)
-weatherContainer.append(date)
-weatherContent.textContent = "icon, temperature, wind, humidity, UV index with color"
-weatherContainer.append(weatherContent)
-forecastContent.textContent = "5-Day Forecast:"
-forecastContainer.append(forecastContent)
-
-
-//adds on click event to search button and appends cities as buttons in the container
-searchButton.addEventListener("submit", function(event) {
-    event.preventDefault();
-    var newCity = document.createElement("button")
-    var userInput = document.querySelector(".form-control").value
-    currentWeatherData(userInput)
-    if (userInput == "") {
-        var blankInput = document.querySelector(".blank-input")
-        blankInput.textContent = "please enter a city"
-    } else {
-        newCity.textContent = userInput
-        buttonContainer.append(newCity)
-    } ClearFields();
-    localStorage.setItem("city", JSON.stringify(userInput))
-})
+//appends city name
+function cityNameAppend() {
+    searchButton.addEventListener("click", function() {
+        cityNameContainer = document.getElementById("city-name-heading")
+        userInput = document.querySelector(".form-control").value
+        cityNameContainer.textContent = userInput
+        clearFields()
+        localStorageData()
+        currentWeatherData()
+        // showData()
+    })
+}
+cityNameAppend()
 
 // clears the input field after search button is clicked
-function ClearFields() {
+function clearFields() {
     document.querySelector(".form-control").value = "";
 }
 
-//displays items in local storage
-buttonContainer.textContent = JSON.parse(localStorage.getItem("city")) || []
+//adds items to local storage and appends them to the button container
+buttonContainer = document.querySelector(".button-container")
+function localStorageData() {
+    localStorage.setItem("city", userInput)
+}
+buttonContainer.textContent = localStorage.getItem("city") || []
 
-//api fetch by city
-function currentWeatherData(cityName) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7acb10b31a225ce5f6e678b28717604c`)
+// //api fetch by city
+function currentWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=7acb10b31a225ce5f6e678b28717604c`)
       .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        var lat = data.coord.lat
-        var lon = data.coord.lon
-        latLonData(lat, lon, cityName)
-      })
-    }
+      .then(data => {console.log(data);
+        lat = data.coord.lat
+        lon = data.coord.lon
+        latLonData(lat, lon)
+    })
+}
 
 //api fetch by lat and lon
-function latLonData(lat, lon, cityName) {
+function latLonData(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=7acb10b31a225ce5f6e678b28717604c`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {console.log(data)
+    })
 }
+
+
